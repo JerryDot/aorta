8d6c4488662d0e329df9bfd5a1e8bff7420a8f42
Daniel Hunt <dansinh64@gmail.com>
2022-01-29 23:43:49 +0000

    try with contribution graph

--- src/screens/ActivityScreen.tsx
+++ src/screens/ActivityScreen.tsx
@@ -3,6 +3,7 @@ import moment from 'moment';
 import React, {useState} from 'react';
 import {Button, Text} from 'react-native';
 import {RootStackParamList} from '../../App';
+import ContributionGraph from '../contribution';
 import {addActivityRecord, getActivityRecords} from '../database/activity';
 import {deleteRecord} from '../database/general';
 
@@ -10,7 +11,7 @@ type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 const ActivityScreen = ({navigation}: Props) => {
   const [dailyRecords, setDailyRecords] = useState(getActivityRecords(moment().startOf('day').toDate()));
-  const buttonOptions = ['sport', 'social', 'dating', 'study', 'alcohol'];
+  const buttonOptions = ['sport', 'social', 'dating', 'study', 'alcohol', 'wfh', 'wfo'];
 
   const refetchDailyRecords = () => {
     setDailyRecords(getActivityRecords(moment().startOf('day').toDate()));
@@ -30,6 +31,31 @@ const ActivityScreen = ({navigation}: Props) => {
     <Button key={type} title={`+ ${type} activitys`} onPress={() => addActivityRecordHandler(type)} />
   );
 
+  const commitsData = [
+    {date: '2017-01-02', count: 1},
+    {date: '2017-01-03', count: 2},
+    {date: '2017-01-04', count: 3},
+    {date: '2017-01-05', count: 4},
+    {date: '2017-01-06', count: 5},
+    {date: '2017-01-30', count: 2},
+    {date: '2017-01-31', count: 3},
+    {date: '2017-03-01', count: 2},
+    {date: '2017-04-02', count: 4},
+    {date: '2017-03-05', count: 2},
+    {date: '2017-02-30', count: 4},
+  ];
+
+  const chartConfig = {
+    // backgroundGradientFrom: '#1E2923',
+    // backgroundGradientFromOpacity: 0,
+    // backgroundGradientTo: '#08130D',
+    // backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+  };
+
   return (
     <>
       <Text>This is {dailyRecords.length} activities.</Text>
@@ -39,6 +65,18 @@ const ActivityScreen = ({navigation}: Props) => {
       {dailyRecords.map(record => (
         <Text onPress={() => deleteActivityRecordHandler(record.recordID)}>{`${record.date} ${record.type} ${record.recordID}`}</Text>
       ))}
+      <ContributionGraph
+        values={commitsData}
+        endDate={new Date('2017-04-05')}
+        numDays={100}
+        // width={screenWidth}
+        height={250}
+        width={600} // chartConfig={chartConfig}
+        tooltipDataAttrs={null}
+        chartConfig={chartConfig}
+        gutterSize={5}
+        showOutOfRangeDays={true}
+      />
     </>
   );
 };
